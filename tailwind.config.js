7f1a6bbd9dd24459107ab23af00cac8c57149b4f
ines luna <inesluna19@gmail.com>
2023-01-28 10:11:48 +0100

    inicializado repo, configuración inicial eslint, tailwind, añadidas dependencias iniciales redux, axios...

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    screens: {
+      xs: '375px',
+      sm: '560px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1600px',
+      '2xl': '1920px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
